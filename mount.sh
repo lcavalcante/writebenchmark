5ac055361a96514444164bd4f08fc8ceacc819bc
Lucas Cavalcante <lucasmcavalcante@gmail.com>
2019-09-03 20:44:11 -0300

    changes to mount - fix

--- mount.sh
+++ mount.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-mapandmount {
-    starttime=$(date +%s%3N)
+mapandmount() {
+    local starttime=$(date +%s%3N)
     rbd map test-"$1"k --pool scalable_sgx --name client.scalable
     mount /dev/rbd/scalable_sgx/test-"$1"k /mnt/test-"$1"k
-    endtime=$(date +%s%3N)
-    diff=$(( $endtime - $starttime ))
-    echo "$i, $diff" >> out-"$max".csv
+    local endtime=$(date +%s%3N)
+    local diff=$(( $endtime - $starttime ))
+    echo "$1, $diff" >> mount-"$max".csv
 }
 
-
-$max = 1000
-for i in {1..$max};
+max=100
+for (( c=1; c<=$max; c++ ))
 do
-    mapandmount $i &
+    mapandmount $c &
 done
--- umount.sh
+++ umount.sh
@@ -2,16 +2,15 @@
 
 unmapandumount() {
     starttime=$(date +%s%3N)
-    umount /dev/rbd/scalable_sgx/test-"$1"k
-    rbd  unmap test-"$1"k --pool scalable_sgx --name client.scalable
+    umount /mnt/test-"$1"k
+    rbd unmap test-"$1"k --pool scalable_sgx --name client.scalable
     endtime=$(date +%s%3N)
     diff=$(( $endtime - $starttime ))
     echo "$1, $diff" >> umount-"$max".csv
 }
 
-max=3
+max=100
 for (( c=1; c<=$max; c++ ))
 do
     unmapandumount $c &
 done
-~
